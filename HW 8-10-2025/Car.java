0c4ad1e78ea1ad31efc91666baea550220ebcabf
ganeshpandi665 <ganeshpandi12012005@gmail.com>
2025-10-09 13:11:49 +0530

    Home Work 8-10-2025

---
+++ HW 8-10-2025/Car.java
@@ -0,0 +1,17 @@
+class Car
+{
+	public static void main(String [] args)
+	{
+		String carName = "Tesla Model 5";
+		String fuelType = "Electric";
+		String brand = "Tesla";
+		short mileage = 396;
+		float price = 85000.00f;
+		
+		System.out.println(carName);
+		System.out.println(fuelType);
+		System.out.println(brand);
+		System.out.println(mileage);
+		System.out.println(price);
+	}
+}
